c3700b310818113ce4e54dbec511eba1315179b5
Tianhua Fang <tianhuaf0526@gmail.com>
2022-05-22 01:12:21 -0700

    Create Constants.java

---
+++ SyncSimulator/DistributedSyncSimulator/Utilities/src/main/java/distributedsyncsimulator/utilities/Constants.java
@@ -0,0 +1,6 @@
+package distributedsyncsimulator.utilities;
+
+public final class Constants{
+    public static final int DETECTION_INTERVAL_MS = 500;
+    public static final String LEAD_NODE_NAME = "LeadNode";
+}
